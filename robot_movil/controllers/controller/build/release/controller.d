3c5a9913dbfe2fcb00b2bbee651f550be2456ff6
Matias Guerrero Castillo <matias.guerrero.c@mail.pucv.cl>
2025-06-23 22:14:18 -0400

    Add basic navigation controller with LIDAR-based obstacle avoidance
    - Implemented main control loop in controller.c using C.
    - Robot moves forward and turns right upon detecting an obstacle closer than 0.2 m.
    - LIDAR sensor used to detect obstacles at the front.
    - GPS sensor used to log X and Z positions during navigation.
    - Motors initialized and controlled via Webots API.
    - Adjusted turn duration via `turn_steps` for smoother avoidance.

--- robot_movil/controllers/controller/build/release/controller.d
+++ robot_movil/controllers/controller/build/release/controller.d
@@ -1,7 +1,8 @@
-build/release/basic_controller.o: basic_controller.c \
+build/release/controller.o: controller.c \
  C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
  C:\Program\ Files\Webots/include/controller/c/webots/types.h \
  C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/gps.h \
  C:\Program\ Files\Webots/include/controller/c/webots/lidar.h \
- C:\Program\ Files\Webots/include/controller/c/webots/lidar_point.h \
- C:\Program\ Files\Webots/include/controller/c/webots/gps.h
+ C:\Program\ Files\Webots/include/controller/c/webots/lidar_point.h
